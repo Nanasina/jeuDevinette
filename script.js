861315cab1e8726f40677b6c49bf412c49d478f4
Nanasina <hasinabefanambinana09@gmail.com>
2025-09-22 21:35:49 +0200

    Ajout d'une fonctionnalité pour rejouer et amélioration du codedans script.js,modification du style.css

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let message = document.getElementById('message');
 let formulaire = document.getElementById('champ');
 let btn_envoyer = document.getElementById('btn_put');
+let btn_restart = document.getElementById('btn_restart');
+let form = document.getElementById('form');
 
 // pour générer aléatoirement un nombre entre 1 et 100
 const nbr_secret = Math.floor(Math.random()*100) + 1; 
@@ -8,6 +10,18 @@ let nbr_tenetative = 0;
 
 btn_envoyer.onclick = function () {
     var p = document.createElement("p"); 
+
+    // vérifie si les nombres saisi sont des entiers comprisent entre 1 et 100
+    if(formulaire.value > 100 || formulaire.value < 0 ){
+        p.innerText = "Veuillez saisir un nombre entier entre 1 et 100";
+        message.appendChild(p);
+        p.style.color = "gray";
+        formulaire.value = "";
+        form.style.display = "none";
+        btn_restart.style.display = "flex";
+        return;
+    }
+    
     // verifier si le champs est vide 
     if(formulaire.value != ""){
 
@@ -20,25 +34,30 @@ btn_envoyer.onclick = function () {
         }
         else if (formulaire.value < nbr_secret) {
             p.innerText = 'Le nombre est plus grand que ' + formulaire.value + '.';
-            message.appendChild(p);
+             message.appendChild(p);
             p.style.color = "gray";
             formulaire.value = "";
             nbr_tenetative += 1;
         } else {
             p.innerText = 'Félicitation le nombre est ' + formulaire.value + '!';
-            message.appendChild(p);
+             message.appendChild(p);
             p.style.color = "green";
-            nbr_tenetative += 1;
+            form.style.display = "none";
+            btn_restart.style.display = "flex";
         }
 
         if(nbr_tenetative === 5){
-            p.innerText = "Game over!"
+            p.innerText = 'Game over,il fallait trouver '+ nbr_secret + '!';
             message.appendChild(p);
             p.style.color = "red";
+            form.style.display = "none";
+            btn_restart.style.display = "flex";
         }
     }else{
        p.innerText = "Le champ est vide!"
        message.appendChild(p);
        p.style.color = "red";
+       form.style.display = "none";
+       btn_restart.style.display = "flex";
     }
 }
